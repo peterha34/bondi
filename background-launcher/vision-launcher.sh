2666b358097a2dccb458a11a5270e0b0778951a5
brontythai <dung.q.thai@student.uts.edu.au>
2017-10-27 18:49:15 +1100

    vision-launcher

---
+++ background-launcher/vision-launcher.sh
@@ -0,0 +1,19 @@
+# !/bin/sh
+# conveyor-launcher.sh
+# Back ground launcher for conveyor pi
+
+
+# sleep to let eveything else kickoff first before starting
+/bin/sleep 20
+
+# navigate to bondi directory, then to this directory, then execute python script, then back home
+cd /
+cd /home/pi/Desktop/bondi/emergency-system
+sudo python switch.py
+cd /
+
+# do the same as above for your own code, change directory locatoin and script name..
+#cd /
+#cd home/pi/Desktop/bondi/xx
+#sudo python xx
+#cd /
